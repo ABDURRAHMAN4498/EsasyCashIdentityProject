05565e0e070dd266685a658224f437274f0949f2
Abdurrahman HacSaid <azazfree1515@gmail.com>
2024-04-24 23:50:40 +0300

    19.ders bitirildi

---
+++ EasyCashIdentityProject.PresentationLayer/ViewComponent/Customer/_CustomerLayoutScriptPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.ViewComponent.Customer
+{
+    public class _CustomerLayoutScriptPartial:Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
